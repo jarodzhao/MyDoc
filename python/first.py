ba92fd68ac0a4903b2c9da4718ca8c93c060ee71
jarodzhao <3245121@qq.com>
2017-10-23 22:43:15 +0800

    python 深入学习

--- python/first.py
+++ python/first.py
@@ -1,16 +1,27 @@
 import printList
 
-txtFile = open('aabb.txt')
 
-for line in txtFile:
-    data = line.split(" ")
-    printList.print_movie(data)
+try:
+    txtFile = open('aabb.txt')
 
-print()
+    for line in txtFile:
+        try:
+            if not line.find(" ") == -1:
+                line = line.split(" ")
+                printList.print_movie(line)
+            else:
+                print(line)
+        except ValueError:
+            print('发布错误,不存在" "')
 
-txtFile.close()
+    #print('jarod zhao',file=txtFile)
 
-''' 单行注释怎么写'''
+    txtFile.close()
+
+except IOError:
+    print('文件读写错误!!!')
+
+''' 单行注释怎么写 '''
 
 nikeName = ['zhao','haitao','jarod',['a',['afaf','dfdf','efef'],'b','c','d']]
 
