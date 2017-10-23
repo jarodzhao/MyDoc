eecbf55c36d96eefe4857071b9d40560ce8bdef4
jarodzhao <3245121@qq.com>
2017-10-23 22:04:35 +0800

    git open() 使用

--- python/first.py
+++ python/first.py
@@ -1,7 +1,17 @@
 import printList
 
+txtFile = open('aabb.txt')
+
+for line in txtFile:
+    data = line.split(" ")
+    printList.print_movie(data)
+
+print()
+
+txtFile.close()
+
 ''' 单行注释怎么写'''
 
 nikeName = ['zhao','haitao','jarod',['a',['afaf','dfdf','efef'],'b','c','d']]
 
-printList.print_movie(nikeName,0)
+#printList.print_movie(nikeName,0)
