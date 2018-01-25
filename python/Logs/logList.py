61c9e59d9a67dd381f5f9767f7a107e54398b9f9
jarodzhao <3245121@qq.com>
2018-01-25 10:07:53 +0800

    删除目录内的python，增加大都会二期洋房毛坯照片

--- python/Logs/logList.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-import LogUtils
-
-logs = open('test.txt','r',encoding='UTF-8')
-
-LogUtils.print_list(logs)
-
-
--- python/file/pomTest.py
+++
@@ -1,8 +0,0 @@
-
-
-pom = open('pom.xml','w')
-
-slist = ['a','b','c','d']
-
-print(slist, file=pom,end='')
-
